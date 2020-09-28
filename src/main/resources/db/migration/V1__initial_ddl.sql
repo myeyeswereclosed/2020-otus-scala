c4d5bd3bceba107236b5174388e4c54c8168a3af
myeyeswereclosed <belovi-tech@mail.ru>
2020-09-28 23:26:53 +0300

    hw03 migration fix

--- src/main/resources/db/migration/V1__initial_ddl.sql
+++ src/main/resources/db/migration/V1__initial_ddl.sql
@@ -2,27 +2,26 @@ begin;
 
 create extension if not exists "uuid-ossp";
 
-delete from flyway_schema_history;
 drop table if exists book_author;
 drop table if exists author;
 drop table if exists comment;
 drop table if exists book;
 
-create table if not exists book(
+create table book(
     id uuid primary key not null default uuid_generate_v4(),
     title varchar(255) not null,
     pages_number int not null,
     year_of_publishing int
 );
 
-create table if not exists author(
+create table author(
     id uuid primary key not null default uuid_generate_v4(),
     first_name varchar(255) not null,
     last_name varchar(255) not null,
     constraint uq__author_first_name_last_name unique (first_name, last_name)
 );
 
-create table if not exists book_author
+create table book_author
 (
     id bigserial primary key not null,
     book_id uuid not null
